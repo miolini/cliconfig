872ba332d4d96e718cb32c1e5ca1c51f8e19a6f3
Artem Andreenko <miolini@gmail.com>
2016-12-03 22:27:14 +0300

    add int flag

--- cliconfig.go
+++ cliconfig.go
@@ -2,6 +2,7 @@ package cliconfig
 
 import (
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/serenize/snaker"
@@ -30,9 +31,23 @@ func Fill(config interface{}, envPrefix string) []cli.Flag {
 				Name:        flagName,
 				EnvVar:      envName,
 				Destination: fieldValue.Addr().Interface().(*string),
+				Value:       fieldType.Tag.Get("default"),
+			}
+			flags = append(flags, flag)
+		case reflect.Int:
+			flag := cli.IntFlag{
+				Name:        flagName,
+				EnvVar:      envName,
+				Destination: fieldValue.Addr().Interface().(*int),
+				Value:       intFromString(fieldType.Tag.Get("default")),
 			}
 			flags = append(flags, flag)
 		}
 	}
 	return flags
+}
+
+func intFromString(s string) int {
+	val, _ := strconv.Atoi(s)
+	return val
 }
